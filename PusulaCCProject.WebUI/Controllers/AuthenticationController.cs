f7e7bea51b1f4cddc0e225f4751d4a43c403ec7e
mehmetmalgir <mmalgr@gmail.com>
2023-06-20 23:22:11 +0300

    BootStrap & DataTables Libraries Added to Project. Register Page Created with modal.

--- PusulaCCProject.WebUI/Controllers/AuthenticationController.cs
+++ PusulaCCProject.WebUI/Controllers/AuthenticationController.cs
@@ -5,6 +5,7 @@ using Newtonsoft.Json;
 using PusulaCCProject.WebUI.Models;
 using PusulaCCProject.WebUI.Validations;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Net.Http;
 using System.Text;
 
@@ -22,7 +23,7 @@ namespace PusulaCCProject.WebUI.Controllers
         public IActionResult LogIn(LoginRegisterModel loginModel)
         {
             LoginRegisterModelValidator validator = new LoginRegisterModelValidator();
-            ValidationResult validationResult = validator.Validate(loginModel);
+            FluentValidation.Results.ValidationResult validationResult = validator.Validate(loginModel);
 
             if (validationResult.IsValid)
             {
@@ -65,7 +66,49 @@ namespace PusulaCCProject.WebUI.Controllers
 
         }
 
+        [HttpPost]
+        public IActionResult Register(LoginRegisterModel loginRegisterModel)
+        {
+            LoginRegisterModelValidator validator = new LoginRegisterModelValidator();
+            FluentValidation.Results.ValidationResult result = validator.Validate(loginRegisterModel);
 
+            if (result.IsValid)
+            {
+                HttpClient client = new HttpClient();
+
+                string jsonContent = JsonConvert.SerializeObject(loginRegisterModel);
+                HttpContent httpContent = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+                var request = client.PostAsync("https://process.pusulacc.com.tr/testapi/api/User/Register", httpContent).Result;
+                var response = request.Content.ReadAsStringAsync().Result;
+
+                var responseModel = JsonConvert.DeserializeObject<OperationResultOfLoginResponse>(response);
+
+                if (responseModel.state == true)
+                {                   
+                    return Json(new { isSuccess = true });
+                }
+                else
+                    return Json(new { isSuccess = false, Message = "Bu kullanıcı adı ile bir kullanıcı mevcut" });
+
+            }
+            else 
+            {
+                string errorMessages = "<div class = 'alert alert-warning'>";
+                foreach (ValidationFailure item in result.Errors)
+                {
+                    errorMessages += $"<div>{item.ErrorMessage}</div>";
+                }
+
+                errorMessages += "</div>";
+
+                return Json(new { isSuccess = false, Message = errorMessages });
+            }
+
+
+
+
+        }
 
 
 
